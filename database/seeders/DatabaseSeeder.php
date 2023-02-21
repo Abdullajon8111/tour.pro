e0aae80fecb4d42909afa7500f00153d4b08c8d7
Farhod Yusupov <61786968+farhodyusupov@users.noreply.github.com>
2023-02-21 17:46:08 +0500

    payment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(AdminUserSeeder::class);
         $this->call(RegionSeeder::class);
+
+        $this->call(PayUzSeeder::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,18 @@ Route::get('language/{locale}', function ($locale) {
 
     return redirect()->back();
 })->name('language.locale');
+
+//handle requests from payment system
+Route::any('/handle/{paysys}',function($paysys){
+    (new Goodoneuz\PayUz\PayUz)->driver($paysys)->handle();
+});
+
+//redirect to payment system or payment form
+Route::any('/pay/{paysys}/{key}/{amount}',function($paysys, $key, $amount){
+    $model = Goodoneuz\PayUz\Services\PaymentService::convertKeyToModel($key);
+    $url = request('redirect_url','/'); // redirect url after payment completed
+    $pay_uz = new Goodoneuz\PayUz\PayUz;
+    $pay_uz
+        ->driver($paysys)
+        ->redirect($model, $amount, 860, $url);
+});
