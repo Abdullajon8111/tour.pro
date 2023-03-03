3e4bdb27548b0ce80b0f23f653fd54c2489460e4
Farhod Yusupov <61786968+farhodyusupov@users.noreply.github.com>
2023-03-03 17:08:04 +0500

    ads ga status qoshish

--- resources/views/tour/column/status.blade.php
+++ resources/views/tour/column/status.blade.php
@@ -47,7 +47,7 @@
 
     @if($entry->status == \App\Models\Tour::STATUS_PUBLISHED)
 
-{{--        @dump($tour->lastAd)--}}
+        @dump($tour->lastAd->isPayed())
 
         @if(!$tour->lastAd || ($tour->lastAd && !$tour->lastAd->isPayed()))
             <a href="{{ route('ads.index', $entry) }}" class="btn btn-sm btn-outline-info mt-1">
